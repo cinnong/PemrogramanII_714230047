7c3b5ba2c2fec527268cec279c9e51de3ff8207e
Dina Oktafiani <dinaoktafiani04@gmail.com>
2024-11-07 19:43:10 +0700

    Practikum & Tugas(sistem manajemen pegawai di perusahaan

---
+++ 714230047/714230047/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _714230047
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
